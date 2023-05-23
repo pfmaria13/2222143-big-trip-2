2cec82182fef5af11e32eda9a166cc87d332496c
pfmaria13 <pfmaria13@gmail.com>
2023-05-24 01:09:14 +0500

    Шаблонизируй это

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -1,23 +1,19 @@
 export default class PointsModel {
-    constructor() {
-      this.points = [];
-    }
-  
-    init(points, destinations, offers) {
-      this.points = points;
-      this.destinations = destinations;
-      this.offers = offers;
-    }
-  
-    getPoints() {
-      return this.points;
-    }
-  
-    getDestinations() {
-      return this.destinations;
-    }
-  
-    getOffers() {
-      return this.offers;
-    }
+  constructor() {
+    this.points = [];
+  } 
+  init(points, destinations, offers) {
+    this.points = points;
+    this.destinations = destinations;
+    this.offers = offers;
   }
+  getPoints() {
+    return this.points;
+  }
+  getDestinations() {
+    return this.destinations;
+  }
+  getOffers() {
+    return this.offers;
+  }
+}
