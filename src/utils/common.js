67f2a49aa28b914b7aff5a594fda3866b9f72e09
pfmaria13 <pfmaria13@gmail.com>
2023-06-08 01:23:23 +0500

    Большие перемены (часть 1)

--- src/utils/common.js
+++ src/utils/common.js
@@ -1,11 +1,28 @@
 const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
+
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
 const getRandomElement = (elements) => {
   const MIN = 0;
   const max = elements.length - 1;
   return elements[getRandomInteger(MIN, max)];
 };
-export { getRandomInteger, getRandomElement };
+
+const updateItem = (items, update) => {
+  const index = items.findIndex((item) => item.id === update.id);
+
+  if (index === -1) {
+    return items;
+  }
+
+  return [
+    ...items.slice(0, index),
+    update,
+    ...items.slice(index + 1),
+  ];
+};
+
+export { getRandomInteger, updateItem, getRandomElement };
